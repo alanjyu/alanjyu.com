b185d54e8614b90731003b73c89ad8c046605ad8
Alan Yu <alanjhyu@gmail.com>
2022-05-11 17:05:43 -0400

    Added 'Experience'

--- src/js/components/cursor.js
+++ src/js/components/cursor.js
@@ -1,51 +1,91 @@
 export default class Cursor {
   constructor() {
-    const targets = document.querySelectorAll('[data-target]');
+    const coverTargets = document.querySelectorAll('[data-target-cover]');
+    const dotTargets = document.querySelectorAll('[data-target-dot]');
     const iframes = document.querySelectorAll('iframe');
     const cursor = document.querySelector('.cursor');
 
-    var targetHover = false;
-    var rect = 0;
-    var cx = 0, cy = 0;
+    var coverTargetHover = false;
+    var dotTargetHover = false;
+    var cx = document.documentElement.clientWidth * .5;
+    var cy = document.documentElement.clientHeight * .5; // dyanmic cursor position
     var hx = 0, hy = 0;
-    var ax = 0, ay = 0;
+    var ax = 0, ay = 0; // adjustments for cursor position
     var bw = 30, bh = 30; // base width and height
-    var cw = bw, ch = bw;
-    const wobble = 20;
+    var cw = bw, ch = bw; // dyanmic cursor width and height
+    const wobble = 10;
+
+    cursor.style.opacity = '0';
 
     document.addEventListener('mousemove', (e) => {
-      // update mouse position
+      // update cursor width and height
       cursor.style.width = cw + 'px';
       cursor.style.height = ch + 'px';
+      cursor.style.opacity = '1';
 
-      if (targetHover) {
+      // cover
+      if (coverTargetHover && (!dotTargetHover)) {
         cx = hx + ax - cw * .5;
         cy = hy + ay - ch * .5;
+        cursor.style.backgroundColor = '#fff';
+        cursor.style.borderRadius = '10px';
+      } else if (dotTargetHover && (!coverTargetHover)) {
+        cx = hx + ax;
+        cy = hy + ay;
         cursor.style.backgroundColor = '#fff';
       } else {
         cx = e.clientX - cw * .5;
         cy = e.clientY - ch * .5;
         cursor.style.backgroundColor = 'transparent';
+        cursor.style.borderRadius = '50%';
       }
       
       cursor.style.transform = 'translate(' + cx + 'px,' + cy + 'px)';
     });
 
-    targets.forEach(target => {
-      target.addEventListener('mousemove', (e) => {
-        targetHover = true;
-        rect = target.getBoundingClientRect();
-        cw = rect.width + 20;
-        ch = rect.height + 20;
+    document.addEventListener('mouseleave', () => {
+      cursor.style.opacity = '0';
+    });
+
+    // covers the entire target
+    coverTargets.forEach(coverTarget => {
+      coverTarget.addEventListener('mousemove', (e) => {
+        coverTargetHover = true;
+        let rect = coverTarget.getBoundingClientRect();
+        cw = rect.width + 15;
+        ch = rect.height + 15;
         hx = (rect.left + rect.right) * .5; // hover center x
         hy = (rect.top + rect.bottom) * .5; // hover center y
         ax = (e.clientX - hx) / cw * wobble; // adjustment on width
         ay = (e.clientY - hy) / ch * wobble; // adjustment on height
       });
 
-      target.addEventListener('mouseleave', (e) => {
-        targetHover = false;
-        rect = 0;
+      coverTarget.addEventListener('mouseleave', (e) => {
+        coverTargetHover = false;
+        cw = bw;
+        ch = bh;
+        hx = 0;
+        hy = 0;
+        ax = 0;
+        ay = 0;
+      });
+    });
+
+    // transform into a dot
+    dotTargets.forEach(dotTarget => {
+      dotTarget.addEventListener('mousemove', (e) => {
+        dotTargetHover = true;
+        let rect = dotTarget.getBoundingClientRect();
+        hx = rect.right - 20;
+        hy = rect.bottom - 20;
+        cw = 5;
+        ch = 5;
+        ax = (e.clientX - (rect.left + rect.right) * .5) * wobble * .005;
+        ay = (e.clientY - (rect.top + rect.bottom) * .5) * wobble * .005;
+      });
+
+      dotTarget.addEventListener('mouseleave', (e) => {
+        dotTargetHover = false;
         cw = bw;
         ch = bh;
         hx = 0;
--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -1,15 +1,24 @@
 export default class Theme {
 	constructor() {
 		var sections = document.querySelectorAll('section');
+		var dimpoint = document.querySelector('#music');
 		var main = document.querySelector('main');
+
+		main.style.backgroundColor = '#f5f5f5';
 
 		sections.forEach(section => {
 			var tp = section.offsetTop; // top position
 			var bg = section.dataset.backgroundcolor; // background color obtained from html
+			var buffer = 75;
 
 			document.addEventListener('scroll', () => {
-				if (window.pageYOffset > tp) {
+				if (window.pageYOffset > (tp - buffer)) {
 					main.style.backgroundColor = bg;
+
+					// if (window.pageYOffset > (dimpoint - buffer)) {
+					// 	main.style.color = '#fff';
+					// 	console.log('trigger!');
+					// }
 				}
 			});
 
