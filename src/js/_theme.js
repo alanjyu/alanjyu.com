07d39e622c26b370102333baaf8402399da5fb9f
Alan Yu <alanjyu@outlook.com>
2024-11-11 11:48:09 +0100

    Fix theme.js error

--- src/js/_theme.js
+++ src/js/_theme.js
@@ -2,7 +2,7 @@ export default class Theme {
   constructor() {
     const themeCheckbox = document.getElementById('checkbox');
     const savedTheme = localStorage.getItem('theme');
-    const themeToApply = savedTheme || getSystemTheme();
+    const themeToApply = savedTheme || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';;
 
     // Apply the theme
     document.documentElement.setAttribute('data-theme', themeToApply);
@@ -10,18 +10,12 @@ export default class Theme {
     // Set checkbox state based on applied theme
     themeCheckbox.checked = themeToApply === 'dark';
     
-    // Get the system's theme preference
-    const getSystemTheme = () => {
-      return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    };
-    
     // Toggle theme and save preference
     themeCheckbox.addEventListener('change', () => {
       const newTheme = themeCheckbox.checked ? 'dark' : 'light';
     
       document.documentElement.setAttribute('data-theme', newTheme);
       localStorage.setItem('theme', newTheme);
-      console.log(localStorage.getItem('theme'))
     });      
   }
 }
