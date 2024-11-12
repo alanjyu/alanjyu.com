9d38d74a708cd3db445a0990a0ec50c2df3f5e71
Alan Yu <alanjhyu@gmail.com>
2024-11-12 01:04:25 +0100

    Darken figures in dark mode

--- src/js/_theme.js
+++ src/js/_theme.js
@@ -1,21 +1,41 @@
 export default class Theme {
   constructor() {
-    const themeCheckbox = document.getElementById('checkbox');
+    const themeCheckbox = document.querySelector('#checkbox');
     const savedTheme = localStorage.getItem('theme');
-    const themeToApply = savedTheme;
+    const backgrounds = document.querySelectorAll('.full-frame');
 
-    // Apply the theme
+    console.log(backgrounds)
+
+    // check the system theme
+    const getSystemTheme = () => {
+      return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    };
+
+    // check if savedTheme exists, if not use the system theme
+    const themeToApply = savedTheme ? savedTheme : getSystemTheme();
+
+    // apply the theme
     document.documentElement.setAttribute('data-theme', themeToApply);
-
-    // Set checkbox state based on applied theme
     themeCheckbox.checked = themeToApply === 'dark';
+    this.toggleBackgroundDarken(themeToApply, backgrounds);
     
-    // Toggle theme and save preference
     themeCheckbox.addEventListener('change', () => {
       const newTheme = themeCheckbox.checked ? 'dark' : 'light';
-    
+  
       document.documentElement.setAttribute('data-theme', newTheme);
       localStorage.setItem('theme', newTheme);
+      this.toggleBackgroundDarken(newTheme, backgrounds);
     });      
+  }
+
+  toggleBackgroundDarken(theme, backgrounds) {
+    // darken backgrounds by adding .background--darken
+    backgrounds.forEach(background => {
+      if (theme === 'dark') {
+        background.classList.add('filter--darken');
+      } else {
+        background.classList.remove('filter--darken');
+      }
+    });
   }
 }
