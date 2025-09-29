b5c903890abf2322e3b429433864e632edcb2e44
Alan Yu <alanjhyu@gmail.com>
2025-09-30 00:32:32 +0200

    Add hover glow effect for some items

--- src/js/_hover.js
+++ src/js/_hover.js
@@ -1,27 +1,50 @@
 export default class Hover {
-    constructor() {
-        this.buttons = document.querySelectorAll('.hover-fill, .hover-glow');
+  constructor() {
+    this.hoverFill = document.querySelectorAll('.hover-fill');
+    this.hoverGlow = document.querySelectorAll('.hover-glow');
 
-        this.buttons.forEach(button => {
-          button.addEventListener('mouseenter', function(e) {
-            const parentOffset = button.getBoundingClientRect();
-            const relX = e.pageX - window.scrollX - parentOffset.left;
-            const relY = e.pageY - window.scrollY - parentOffset.top;
-      
-            // Set CSS variables for `::after` positioning
-            button.style.setProperty('--hover-x', `${relX}px`);
-            button.style.setProperty('--hover-y', `${relY}px`);
-          });
-      
-          button.addEventListener('mouseout', function(e) {
-            const parentOffset = button.getBoundingClientRect();
-            const relX = e.pageX - window.scrollX - parentOffset.left;
-            const relY = e.pageY - window.scrollY - parentOffset.top;
-      
-            // Update `--hover-x` and `--hover-y` variables on mouse exit
-            button.style.setProperty('--hover-x', `${relX}px`);
-            button.style.setProperty('--hover-y', `${relY}px`);
-          });
-        });
-    }
+    this.hoverFill.forEach(filler => {
+      filler.addEventListener('mouseenter', function(e) {
+        let parentOffset = filler.getBoundingClientRect();
+        let relX = e.pageX - window.scrollX - parentOffset.left;
+        let relY = e.pageY - window.scrollY - parentOffset.top;
+  
+        // Set CSS variables for positioning
+        filler.style.setProperty('--hover-x', `${relX}px`);
+        filler.style.setProperty('--hover-y', `${relY}px`);
+      });
+  
+      filler.addEventListener('mouseout', function(e) {
+        let parentOffset = filler.getBoundingClientRect();
+        let relX = e.pageX - window.scrollX - parentOffset.left;
+        let relY = e.pageY - window.scrollY - parentOffset.top;
+  
+        // Update `--hover-x` and `--hover-y` variables on mouse exit
+        filler.style.setProperty('--hover-x', `${relX}px`);
+        filler.style.setProperty('--hover-y', `${relY}px`);
+      });
+    });
+
+    this.hoverGlow.forEach(glower => {
+      glower.addEventListener('mousemove', function(e) {
+        let parentOffset = glower.getBoundingClientRect();
+        let relX = e.pageX - window.scrollX - parentOffset.left;
+        let relY = e.pageY - window.scrollY - parentOffset.top;
+  
+        // Set CSS variables for positioning
+        glower.style.setProperty('--hover-x', `${relX}px`);
+        glower.style.setProperty('--hover-y', `${relY}px`);
+      });
+  
+      glower.addEventListener('mouseout', function(e) {
+        let parentOffset = glower.getBoundingClientRect();
+        let relX = e.pageX - window.scrollX - parentOffset.left;
+        let relY = e.pageY - window.scrollY - parentOffset.top;
+  
+        // Update `--hover-x` and `--hover-y` variables on mouse exit
+        glower.style.setProperty('--hover-x', `${relX}px`);
+        glower.style.setProperty('--hover-y', `${relY}px`);
+      });
+    });
+  }
 }
